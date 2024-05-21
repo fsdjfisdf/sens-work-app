b35226a871bef7642681878627c58d730f507f38
jhw <jhw86399@naver.com>
2024-05-21 17:39:07 +0900

    saved11

--- back/index.js
+++ back/index.js
@@ -1,6 +1,7 @@
-const express = require('./config/express');
+const expressApp = require('./config/express');
 const { logger } = require('./config/winston'); // log
 
 const port = 3001; // 포트 번호 확인
-express().listen(port);
-logger.info(`API Server Start At Port ${port}`);
+expressApp.listen(port, () => {
+  logger.info(`API Server Start At Port ${port}`);
+});
