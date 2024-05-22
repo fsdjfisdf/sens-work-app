f2b3574277d651106f6c80a8cc02581fa6c0eed2
jhw <jhw86399@naver.com>
2024-05-22 18:04:46 +0900

    saved11

--- front/js/autosetup.js
+++ front/js/autosetup.js
@@ -162,19 +162,19 @@ document.addEventListener('DOMContentLoaded', function() {
         resetAndFillFields('task_result', ['TTTM 완료']);
       }
     }
-    
+
     function resetAndFillFields(field, values) {
         const container = document.getElementById(`${field}Fields`);
         container.innerHTML = ''; // 기존 필드 초기화
         values.forEach(value => addField(field, value));
       }
     
-      function addField(field, value) {
+      function addField(field, value = '') {
         const container = document.getElementById(`${field}Fields`);
         const newField = document.createElement('textarea');
         newField.name = `${field}[]`; // 배열로 전송하기 위해 name 속성을 배열 형태로 변경
         newField.className = `${field}-input`;
-        newField.value = value || '';
+        newField.value = value;
         newField.required = true;
         container.appendChild(newField);
         updateRemoveButtonState(field);
