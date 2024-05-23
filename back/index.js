901145e6ec6870c043afa05781846831ad405c9b
jhw <jhw86399@naver.com>
2024-05-23 16:48:01 +0900

    saved11

--- back/index.js
+++ back/index.js
@@ -1,7 +1,3 @@
-delete require.cache[require.resolve('./config/secret')];
-const secret = require('./config/secret');
-console.log('Secret Config:', secret);
-
 const expressApp = require('./config/express');
 const { logger } = require('./config/winston');
 
