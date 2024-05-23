6bc66bb7cd34a59a9603b6c382a704262ea9c180
jhw <jhw86399@naver.com>
2024-05-23 15:38:10 +0900

    saved11

--- back/src/routes/indexRoute.js
+++ back/src/routes/indexRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController'); // userController로 변경된 경우
+const userController = require('../controllers/userController');
 
 // 사용자 회원가입
 router.post('/register', userController.register);
@@ -8,7 +8,6 @@ router.post('/register', userController.register);
 // 사용자 로그인
 router.post('/login', userController.login);
 
-// 작업 이력 조회 및 추가 (필요에 따라)
 const workLogController = require('../controllers/workLogController'); // 경로 및 파일 이름 확인
 router.get('/work-logs', workLogController.getWorkLogs);
 router.post('/work-logs', workLogController.addWorkLog);
