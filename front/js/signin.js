97f1f64963fa1aa17441d0fb631624c276672f6a
jhw <jhw86399@naver.com>
2024-05-23 13:17:03 +0900

    saved11

--- front/js/signin.js
+++ front/js/signin.js
@@ -1,35 +1,48 @@
+
+/* 
+로그인 API 연동
+
+1. #signin 클릭
+2. #userID, #password 값 확인 (두 값이 모두 입력 되어 있지 않으면 return)
+3. 로그인 API 요청
+4. 요청이 성공적이지 않다면, alert message
+5. 요청이 성공하면, jwt를 localstorage에 저장하고 main page 이동
+
+*/
+
 const btnSignIn = document.querySelector("#signin");
 
+// 1. #signin 클릭
 btnSignIn.addEventListener("click", signIn);
 
 async function signIn(event) {
   const userID = document.querySelector("#userID").value;
   const password = document.querySelector("#password").value;
 
+  // 2. #email, #password 값 확인 (두 값이 모두 입력 되어 있지 않으면 return)
   if (!userID || !password) {
     return alert("회원 정보를 입력해주세요.");
   }
 
-  try {
-    const signInReturn = await axios({
-      method: "post",
-      url: url + "/sign-in", // URL이 중복되지 않도록 확인
-      data: { userID: parseInt(userID), password: password }, // userID를 정수형으로 변환
-    });
+  // 3. 로그인 API 요청
+  const signInReturn = await axios({
+    method: "post", // http method
+    url: url + "/sign-in",
+    headers: {}, // packet header
+    data: { userID: userID, password: password }, // packet body
+  });
 
-    const isValidSignIn = signInReturn.data.code == 200;
+  // 4. 요청이 성공적이지 않다면, alert message
+  const isValidSignIn = signInReturn.data.code == 200;
 
-    if (!isValidSignIn) {
-      return alert(signInReturn.data.message);
-    }
-
-    const jwt = signInReturn.data.result.jwt;
-    localStorage.setItem("x-access-token", jwt);
-    alert(signInReturn.data.message);
-
-    location.replace("./index.html");
-  } catch (error) {
-    console.error(error);
-    alert("로그인 중 오류가 발생했습니다.");
+  if (!isValidSignIn) {
+    return alert("요청에 문제가 생겼습니다.");
   }
+
+  // 5. 요청이 성공하면, jwt를 localstorage에 저장하고 main page 이동
+  const jwt = signInReturn.data.result.jwt;
+  localStorage.setItem("x-access-token", jwt);
+  alert(signInReturn.data.message);
+
+  return location.replace("./index.html");
 }
