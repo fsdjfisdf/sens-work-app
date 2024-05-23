b74cc8ed718711c09db219e77ada1dbe303c3b0c
jhw <jhw86399@naver.com>
2024-05-23 13:36:11 +0900

    saved11

--- back/config/express.js
+++ back/config/express.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const compression = require("compression");
+const methodOverride = require("method-override");
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const { pool } = require('./database');
 const { logger } = require('./winston');
+var cors = require("cors");
 
 const app = express();
 
@@ -75,14 +78,24 @@ app.get('/logs', async (req, res) => {
   }
 });
 
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
+
+
+
+module.exports = function () {
+  const app = express();
+
+  /* 미들웨어 설정 */
+  app.use(compression()); // HTTP 요청을 압축 및 해제
+  app.use(express.json()); // body값을 파싱
+  app.use(express.urlencoded({ extended: true })); // form 으로 제출되는 값 파싱
+  app.use(methodOverride()); // put, delete 요청 처리
+  app.use(cors()); // 웹브라우저 cors 설정을 관리
+  app.use(express.static("/home/ubuntu/food-map-dist-example/front")); // express 정적 파일 제공 (html, css, js 등..)
+  // app.use(express.static(process.cwd() + '/public'));
+
+  /* 직접 구현해야 하는 모듈 */
+  require("../src/routes/indexRoute")(app);
+
+  return app;
+};
