a4a0ff90346f8eedf8c5ee3ac1379faf644bc7f2
jhw <jhw86399@naver.com>
2024-05-23 10:37:43 +0900

    saved11

--- back/src/routes/indexRoute.js
+++ back/src/routes/indexRoute.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const workLogController = require('../controllers/workLogController');
+const indexController = require('../controllers/indexController');
 
 // 작업 이력 조회
 router.get('/work-logs', workLogController.getWorkLogs);
 
 // 작업 이력 추가
 router.post('/work-logs', workLogController.addWorkLog);
+
+router.post('/sign-in', indexController.createJwt);
 
 module.exports = router;
