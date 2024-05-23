06953feb49db37c5f9e4d8a1c83743e6b6ec6cc8
jhw <jhw86399@naver.com>
2024-05-23 17:25:29 +0900

    saved11

--- front/js/signup.js
+++ front/js/signup.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       if (response.status === 201) {
         alert('회원가입이 성공적으로 완료되었습니다.');
-        window.location.href = './login.html'; // 로그인 페이지로 리디렉션
+        window.location.href = './signin.html'; // 로그인 페이지로 리디렉션
       } else {
         alert('회원가입 중 오류가 발생했습니다.');
       }
