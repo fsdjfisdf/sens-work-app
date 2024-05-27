9f51b039eaa2938deceae341542558f855e517a7
jhw <jhw86399@naver.com>
2024-05-27 15:31:12 +0900

    saved11

--- back/src/dao/indexDao.js
+++ back/src/dao/indexDao.js
@@ -1,31 +1,117 @@
-const { pool } = require('../config/database');
+const { pool } = require("../../config/database");
 
-exports.getWorkLogs = async () => {
-  const connection = await pool.getConnection(async conn => conn);
-  try {
-    const [rows] = await connection.query(`SELECT * FROM work_log`);
-    connection.release();
-    return rows;
-  } catch (err) {
-    connection.release();
-    throw new Error(`Error retrieving work logs: ${err.message}`);
-  }
+// 로그인 (회원검증)
+exports.isValidUsers = async function (connection, userID, password) {
+  const Query = `SELECT userIdx, nickname FROM Users where userID = ? and password = ? and status = 'A';`;
+  const Params = [userID, password];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
 };
 
-exports.addWorkLog = async (task_name, worker, task_result, task_cause, task_description, task_date, start_time, end_time, none_time, move_time, group, site, line, equipment_type, equipment_name, work_type, setup_item, status) => {
-  const connection = await pool.getConnection(async conn => conn);
-  try {
-    const query = `
-      INSERT INTO work_log (
-        task_name, worker, task_result, task_cause, task_description, task_date, start_time, end_time, none_time, move_time, \`group\`, site, line, equipment_type, equipment_name, work_type, setup_item, status
-      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
-    const values = [task_name, worker, task_result, task_cause, task_description, task_date, start_time, end_time, none_time, move_time, group, site, line, equipment_type, equipment_name, work_type, setup_item, status];
-    await connection.query(query, values);
-  } catch (err) {
-    throw new Error(`Error adding work log: ${err.message}`);
-  } finally {
-    connection.release();
-  }
+// 회원가입
+exports.insertUsers = async function (connection, userID, password, nickname, group, site, level, hireDate, mainSetUpCapa, mainMaintCapa, mainCapa, multiSetUpCapa, multiMaintCapa, multiCapa, totalCapa) {
+  const Query = `insert into Users(userID, password, nickname, \`group\`, site, level, hire_date, main_set_up_capa, main_maint_capa, main_capa, multi_set_up_capa, multi_maint_capa, multi_capa, total_capa) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?);`;
+  const Params = [userID, password, nickname, group, site, level, hireDate, mainSetUpCapa, mainMaintCapa, mainCapa, multiSetUpCapa, multiMaintCapa, multiCapa, totalCapa];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
 };
 
+// 회원 정보 조회
+exports.getUserById = async function (connection, userIdx) {
+  const Query = `SELECT userID, nickname, \`group\`, site, level, hire_date, main_set_up_capa, main_maint_capa, main_capa, multi_set_up_capa, multi_maint_capa, multi_capa, total_capa FROM Users WHERE userIdx = ? AND status = 'A';`;
+  const Params = [userIdx];
+
+  const [rows] = await connection.query(Query, Params);
+  return rows;
+};
+
+
+exports.selectRestaurants = async function (connection, category) {
+  const selectAllRestaurantsQuery = `SELECT title, address, category, videoUrl FROM Restaurants where status = 'A';`;
+  const selectCategorizedRestaurantsQuery = `SELECT title, address, category, videoUrl FROM Restaurants where status = 'A' and category = ?;`;
+
+  const Params = [category];
+
+  const Query = category
+    ? selectCategorizedRestaurantsQuery
+    : selectAllRestaurantsQuery;
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+exports.deleteStudent = async function (connection, studentIdx) {
+  const Query = `update Students set status = "D" where studentIdx = ?;`;
+  const Params = [studentIdx];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+exports.updateStudents = async function (
+  connection,
+  studentIdx,
+  studentName,
+  major,
+  birth,
+  address
+) {
+  const Query = `update Students set studentName = ifnull(?, studentName), major = ifnull(?, major), birth = ifnull(?, birth), address = ifnull(?, address) where studentIdx = ?;`;
+  const Params = [studentName, major, birth, address, studentIdx];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+exports.isValidStudentIdx = async function (connection, studentIdx) {
+  const Query = `SELECT * FROM Students where studentIdx = ? and status = 'A';`;
+  const Params = [studentIdx];
+
+  const [rows] = await connection.query(Query, Params);
+
+  if (rows < 1) {
+    return false;
+  }
+
+  return true;
+};
+
+exports.insertStudents = async function (
+  connection,
+  studentName,
+  major,
+  birth,
+  address
+) {
+  const Query = `insert into Students(studentName, major, birth, address) values (?,?,?,?);`;
+  const Params = [studentName, major, birth, address];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+exports.selectStudents = async function (connection, studentIdx) {
+  const Query = `SELECT * FROM Students where studentIdx = ?;`;
+  const Params = [studentIdx];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+exports.exampleDao = async function (connection) {
+  const Query = `SELECT * FROM Students;`;
+  const Params = [];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
