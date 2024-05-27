ff8c3122607fdc4a677caa5c1fe549a896d07eeb
jhw <jhw86399@naver.com>
2024-05-27 17:18:57 +0900

    saved11

--- back/src/controllers/workLogController.js
+++ back/src/controllers/workLogController.js
@@ -1,8 +1,8 @@
-const workLogDao = require('../dao/workLogDao');
+const work_LogDao = require('../dao/work_LogDao');
 
 exports.getWorkLogs = async (req, res) => {
   try {
-    const logs = await workLogDao.getWorkLogs();
+    const logs = await work_LogDao.getWorkLogs();
     res.status(200).json(logs);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -15,11 +15,10 @@ exports.addWorkLog = async (req, res) => {
     group, site, line, equipment_type, equipment_name, workType, setupItem, status
   } = req.body;
 
-  // task_man 필드를 배열 형식에서 문자열 형식으로 변환
   const formattedTaskMan = task_man.map(man => `${man.name}(${man.type})`).join(', ');
 
   try {
-    await workLogDao.addWorkLog(
+    await work_LogDao.addWorkLog(
       task_name, worker, task_result, task_cause, formattedTaskMan, task_description, task_date, start_time, end_time, none_time, move_time,
       group, site, line, equipment_type, equipment_name, workType, setupItem, status
     );
