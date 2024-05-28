79e28fd7c1dcca2bb4fc7787287db9f076979212
jhw <jhw86399@naver.com>
2024-05-28 11:28:26 +0900

    saved11

--- front/js/worklog.js
+++ front/js/worklog.js
@@ -25,11 +25,14 @@ document.addEventListener('DOMContentLoaded', async () => {
     const taskCauses = Array.from(document.getElementsByClassName('task-cause-input')).map(input => input.value).join('\n');
 
     // 여러 task_man 값을 역할과 함께 결합
-    const taskMans = Array.from(document.querySelectorAll('.task-man-container')).map((container, index) => {
+    let taskMans = Array.from(document.querySelectorAll('.task-man-container')).map((container, index) => {
       const input = container.querySelector('.task-man-input').value;
       const role = container.querySelector('.task-man-select').value;
       return `${input}(${role})`;
-    }).join(' ');
+    });
+
+    // 중복 제거 로직
+    taskMans = [...new Set(taskMans)].join(' ');
 
     // 여러 task_description 값을 줄바꿈으로 결합
     const taskDescriptions = Array.from(document.getElementsByClassName('task-description-input')).map(input => input.value).join('\n');
