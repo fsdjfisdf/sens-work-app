08784ec6da7c868a9326cde9b0e468ec4af4961e
jhw <jhw86399@naver.com>
2024-05-28 09:21:28 +0900

    saved11

--- front/js/addandremove.js
+++ front/js/addandremove.js
@@ -1,15 +1,15 @@
 // JavaScript 파일 (addandremove.js)
 document.addEventListener('DOMContentLoaded', function() {
-  function setupDynamicFields(containerId, inputClass) {
+  function setupDynamicFields(containerId, inputClass, template) {
     const container = document.getElementById(containerId);
     const addButton = container.querySelector(`#add-${inputClass}`);
     const removeButton = container.querySelector(`#remove-${inputClass}`);
 
     addButton.addEventListener('click', function() {
-      const newInput = document.createElement('textarea');
-      newInput.name = inputClass;
-      newInput.className = inputClass + '-input';
-      container.insertBefore(newInput, addButton);
+      const newField = document.createElement('div');
+      newField.className = 'task-man-container';
+      newField.innerHTML = template;
+      container.insertBefore(newField, addButton);
 
       if (container.querySelectorAll(`.${inputClass}-input`).length > 1) {
         removeButton.disabled = false;
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     removeButton.addEventListener('click', function() {
-      const inputs = container.querySelectorAll(`.${inputClass}-input`);
+      const inputs = container.querySelectorAll(`.${inputClass}-container`);
       if (inputs.length > 1) {
         container.removeChild(inputs[inputs.length - 1]);
         if (inputs.length === 2) {
@@ -27,8 +27,16 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  setupDynamicFields('task-results-container', 'task-result');
-  setupDynamicFields('task-causes-container', 'task-cause');
-  setupDynamicFields('task-mans-container', 'task-man');
-  setupDynamicFields('task-descriptions-container', 'task-description');
+  const taskManTemplate = `
+    <textarea name="task_man" class="task-man-input" required></textarea>
+    <select name="task_man_role" class="task-man-select" required>
+      <option value="main">main</option>
+      <option value="support">support</option>
+    </select>
+  `;
+
+  setupDynamicFields('task-results-container', 'task-result', '<textarea name="task_result" class="task-result-input" required></textarea>');
+  setupDynamicFields('task-causes-container', 'task-cause', '<textarea name="task_cause" class="task-cause-input" required></textarea>');
+  setupDynamicFields('task-mans-container', 'task-man', taskManTemplate);
+  setupDynamicFields('task-descriptions-container', 'task-description', '<textarea name="task_description" class="task-description-input" required></textarea>');
 });
