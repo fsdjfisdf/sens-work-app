c7841b1478a669e4cd609019c2c3ee8e0d4478be
jhw <jhw86399@naver.com>
2024-05-31 10:03:36 +0900

    saved11

--- back/config/express.js
+++ back/config/express.js
@@ -56,6 +56,7 @@ module.exports = function () {
   
     // 이 부분에 로그 추가
     logger.info('maint_item 값:', maint_item);
+    logger.info('setupItem 값:', setupItem);  // setupItem 값 로그 추가
     logger.info('task_maint 값:', task_maint);
   
     const taskResult = task_result || '';
