ee0de9d37c3707e97afd27d41125ae2148ec52d1
jhw <jhw86399@naver.com>
2024-05-31 15:11:19 +0900

    saved11

--- front/js/printInform.js
+++ front/js/printInform.js
@@ -1,105 +1,117 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('print-inform').addEventListener('click', () => {
-      const taskName = document.getElementById('task_name').value;
-      const status = document.getElementById('status').value;
-  
-      const taskResults = Array.from(document.getElementsByClassName('task-result-input'))
-        .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
-        .join('\n');
-      
-      const taskCauses = Array.from(document.getElementsByClassName('task-cause-input'))
-        .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
-        .join('\n');
+        const taskName = document.getElementById('task_name').value;
+        const status = document.getElementById('status').value;
+        const taskResults = Array.from(document.getElementsByClassName('task-result-input'))
+            .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
+            .join('\n');
 
-      const taskMans = Array.from(document.getElementsByClassName('task-man-input'))
-        .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
-        .join('\n');
-  
-      const taskDescriptions = Array.from(document.getElementsByClassName('task-description-input'))
-        .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
-        .join('\n');
-  
-      const taskDate = document.getElementById('task_date').value;
-      const startTime = document.getElementById('start_time').value;
-      const endTime = document.getElementById('end_time').value;
-      const noneTime = document.getElementById('noneTime').value;
-      const moveTime = document.getElementById('moveTime').value;
-  
-      const informContent = `
-        <strong>${taskName}</strong><br><br>
-        1) STATUS<br>
-        -. ${status}<br><br>
-        2) ACTION<br>
-        ${taskDescriptions.split('\n').join('<br>')}<br><br>
-        3) CAUSE<br>
-        ${taskCauses.split('\n').join('<br>')}<br><br>
-        ${taskMans.split('\n').join('<br>')}<br><br>
-        4) RESULT<br>
-        ${taskResults.split('\n').join('<br>')}<br><br>
-        작업자: ${startTIme}<br>
-        작업시간: ${startTime} - ${endTime}<br>
-        (None: ${noneTime}, Move: ${moveTime})<br>
-      `;
-  
-      const printContainer = document.getElementById('print-container');
-      printContainer.innerHTML = informContent;
+        const taskCauses = Array.from(document.getElementsByClassName('task-cause-input'))
+            .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
+            .join('\n');
+
+        const taskDescriptions = Array.from(document.getElementsByClassName('task-description-input'))
+            .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
+            .join('\n');
+
+        const taskDate = document.getElementById('task_date').value;
+        const startTime = document.getElementById('start_time').value;
+        const endTime = document.getElementById('end_time').value;
+        const noneTime = document.getElementById('noneTime').value;
+        const moveTime = document.getElementById('moveTime').value;
+        const taskSOP = document.getElementById('SOP').value;
+        const taskTSGuide = document.getElementById('tsguide').value;
+
+        const taskMans = Array.from(document.querySelectorAll('.task-man-container .task-man-input'))
+            .map(input => input.value)
+            .join(', ');
+
+        const informContent = `
+            <strong>${taskName}</strong><br><br>
+            1) STATUS<br>
+            -. ${status}<br><br>
+            2) ACTION<br>
+            ${taskDescriptions.split('\n').join('<br>')}<br><br>
+            3) CAUSE<br>
+            ${taskCauses.split('\n').join('<br>')}<br><br>
+            4) RESULT<br>
+            ${taskResults.split('\n').join('<br>')}<br><br>
+            5) SOP 및 T/S Guide 활용<br>
+            ${taskSOP} / ${taskTSGuide}<br><br>
+            작업자: ${taskMans.split(',')}<br><br>
+            작업시간: ${startTime} - ${endTime}<br>
+            (None ${noneTime}, Move ${moveTime})<br>
+        `;
+
+        const printWindow = window.open('', '', 'width=800,height=600');
+        printWindow.document.write('<html><head><title>작업 이력</title></head><body>');
+        printWindow.document.write(informContent);
+        printWindow.document.write('</body></html>');
+        printWindow.document.close();
+        
+        // 창이 포커스를 잃을 때 창 닫기 이벤트 추가
+        printWindow.onblur = function() {
+            printWindow.close();
+        };
     });
-  
+
     document.getElementById('copy-inform').addEventListener('click', () => {
-      const taskName = document.getElementById('task_name').value;
-      const status = document.getElementById('status').value;
-  
-      const taskResults = Array.from(document.getElementsByClassName('task-result-input'))
-        .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
-        .join('\n');
-      
-      const taskCauses = Array.from(document.getElementsByClassName('task-cause-input'))
-        .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
-        .join('\n');
+        const taskName = document.getElementById('task_name').value;
+        const status = document.getElementById('status').value;
+        const taskResults = Array.from(document.getElementsByClassName('task-result-input'))
+            .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
+            .join('\n');
 
-      const taskMans = Array.from(document.getElementsByClassName('task-man-input'))
-        .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
-        .join('\n');
-  
-      const taskDescriptions = Array.from(document.getElementsByClassName('task-description-input'))
-        .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
-        .join('\n');
-  
-      const taskDate = document.getElementById('task_date').value;
-      const startTime = document.getElementById('start_time').value;
-      const endTime = document.getElementById('end_time').value;
-      const noneTime = document.getElementById('noneTime').value;
-      const moveTime = document.getElementById('moveTime').value;
-  
-      const informContent = `
-  ${taskName}
-  
-  1) STATUS
-  -. ${status}
-  
-  2) ACTION
-  ${taskDescriptions.split('\n').join('\n')}
-  
-  3) CAUSE
-  ${taskCauses.split('\n').join('\n')}
-  ${taskMans.split('\n').join('\n')}
-  
-  4) RESULT
-  ${taskResults.split('\n').join('\n')}
-  
-  작업자: ${startTIme}
-  작업시간: ${startTime} - ${endTime}
-  (None: ${noneTime}, Move: ${moveTime})
-      `;
-  
-      const tempTextArea = document.createElement('textarea');
-      tempTextArea.value = informContent.trim();
-      document.body.appendChild(tempTextArea);
-      tempTextArea.select();
-      document.execCommand('copy');
-      document.body.removeChild(tempTextArea);
-  
-      alert('작업 이력 정보가 복사되었습니다.');
+        const taskCauses = Array.from(document.getElementsByClassName('task-cause-input'))
+            .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
+            .join('\n');
+
+        const taskDescriptions = Array.from(document.getElementsByClassName('task-description-input'))
+            .map(input => input.value.split('\n').map(line => `-. ${line}`).join('\n'))
+            .join('\n');
+
+        const taskDate = document.getElementById('task_date').value;
+        const startTime = document.getElementById('start_time').value;
+        const endTime = document.getElementById('end_time').value;
+        const noneTime = document.getElementById('noneTime').value;
+        const moveTime = document.getElementById('moveTime').value;
+        const taskSOP = document.getElementById('SOP').value;
+        const taskTSGuide = document.getElementById('tsguide').value;
+
+        const taskMans = Array.from(document.querySelectorAll('.task-man-container .task-man-input'))
+            .map(input => input.value)
+            .join(', ');
+
+        const informContent = `
+${taskName}
+
+1) STATUS
+-. ${status}
+
+2) ACTION
+${taskDescriptions.split('\n').join('\n')}
+
+3) CAUSE
+${taskCauses.split('\n').join('\n')}
+
+4) RESULT
+${taskResults.split('\n').join('\n')}
+
+5) SOP 및 T/S Guide 활용
+${taskSOP} / ${taskTSGuide}
+
+작업자: ${taskMans.split(',')}
+작업시간: ${startTime} - ${endTime}
+(None ${noneTime}, Move ${moveTime})
+        `;
+
+        const tempTextArea = document.createElement('textarea');
+        tempTextArea.value = informContent.trim();
+        document.body.appendChild(tempTextArea);
+        tempTextArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempTextArea);
+
+        alert('작업 이력 정보가 복사되었습니다.');
     });
-  });
-  
+});
