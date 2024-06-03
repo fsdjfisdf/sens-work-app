dc7a1f50d3fb153f0a234bae6ba441802e4e7b71
jhw <jhw86399@naver.com>
2024-06-03 11:26:23 +0900

    saved15

--- back/src/routes/worklogRoute.js
+++ back/src/routes/worklogRoute.js
@@ -11,7 +11,4 @@ router.post('/work-logs', workLogController.addWorkLog);
 // 작업 이력 삭제
 router.delete('/work-logs/:id', workLogController.deleteWorkLog);
 
-// 작업 이력 수정
-router.put('/work-logs/:id', workLogController.updateWorkLog);
-
 module.exports = router;
