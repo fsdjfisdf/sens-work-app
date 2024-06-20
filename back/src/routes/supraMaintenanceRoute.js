793be51e731139eac346cdc1e05dab7f17049ef7
jhw <jhw86399@naver.com>
2024-06-20 10:41:38 +0900

    saved15

--- back/src/routes/supraMaintenanceRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const supraMaintenanceController = require('../controllers/supraMaintenanceController');
-
-// SUPRA Maintenance Checklist 저장
-router.post('/supra-maintenance', supraMaintenanceController.saveChecklist);
-
-module.exports = router;
