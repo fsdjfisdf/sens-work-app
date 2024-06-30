3cc83f54bf1cda7786545f30cf0925a954de8bca
jhw <jhw86399@naver.com>
2024-07-01 08:48:55 +0900

    saved18

--- front/js/site.js
+++ front/js/site.js
@@ -8,7 +8,7 @@ document.getElementById('site').addEventListener('change', function() {
     // SITE에 따른 LINE 옵션 정의
     const lineOptions = {
       "PT": ["P1F", "P1D", "P2F", "P2D", "P2-S5", "P3F", "P3D", "P3-S5"],
-      "HS": ["12L", "13L", "15L", "16L", "17L", "S1", "S3", "S4", "V1", "NRD", "NRD-V", "U4", "M1"],
+      "HS": ["12L", "13L", "15L", "16L", "17L", "S1", "S3",  "S4", "S3V", "NRD", "NRD-V", "U4", "M1", "5L"],
       "IC": ["M14", "M16"],
       "CJ": ["M11", "M12", "M15"],
       "PSKH": ["PSKH"] // PSKH의 경우 선택할 수 있는 하나의 옵션만 존재
