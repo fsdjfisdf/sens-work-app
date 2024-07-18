6e489ae1bcb78558d5847d142b168b2823298ced
jhw <jhw86399@naver.com>
2024-07-18 10:28:39 +0900

    saved18

--- back/src/controllers/workLogController.js
+++ back/src/controllers/workLogController.js
@@ -71,19 +71,13 @@ exports.exportWorkLogs = async (req, res) => {
 
 exports.updateWorkLog = async (req, res) => {
     const { id } = req.params;
-    const {
-        task_name, task_result, task_cause, task_man, task_description, task_date, start_time, end_time, none_time, move_time,
-        group, site, SOP, tsguide, line, warranty, equipment_type, equipment_name, workType, setupItem, maintItem, transferItem, task_maint, status
-    } = req.body;
+    const updatedFields = req.body;
 
     console.log(`Received update request for ID: ${id}`);
     console.log('Updated log data:', req.body);
 
     try {
-        await workLogDao.updateWorkLog(
-            id, task_name, task_result, task_cause, task_man, task_description, task_date, start_time, end_time, none_time, move_time,
-            group, site, SOP, tsguide, line, warranty, equipment_type, equipment_name, workType, setupItem, maintItem, transferItem, task_maint, status
-        );
+        await workLogDao.updateWorkLog(id, updatedFields);
         res.status(200).json({ message: "Work log updated" });
     } catch (err) {
         console.error(`Error updating work log with ID: ${id}`, err);
