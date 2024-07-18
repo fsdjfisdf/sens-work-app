fcf456128392e49042a7f846819ed06bd71cf685
jhw <jhw86399@naver.com>
2024-07-18 10:22:03 +0900

    saved18

--- back/src/dao/work_LogDao.js
+++ back/src/dao/work_LogDao.js
@@ -43,26 +43,53 @@ exports.deleteWorkLog = async (id) => {
   }
 };
 
-const { pool } = require('../config/database');
 
 exports.updateWorkLog = async (id, task_name, task_result, task_cause, task_man, task_description, task_date, start_time, end_time, none_time, move_time,
     group, site, SOP, tsguide, line, warranty, equipment_type, equipment_name, workType, setupItem, maintItem, transferItem, task_maint, status) => {
 
-    const values = [
-        task_name || null, task_result || null, task_cause || null, task_man || null, task_description || null, task_date || null, start_time || null, end_time || null, none_time || null, move_time || null,
-        group || null, site || null, SOP || null, tsguide || null, line || null, warranty || null, equipment_type || null, equipment_name || null, workType || null, setupItem || null, maintItem || null, transferItem || null, task_maint || null, status || null, id
-    ];
+    const fields = [];
+    const values = [];
 
-    console.log('작업 로그 수정 쿼리:');
+    if (task_name !== undefined) { fields.push("task_name = ?"); values.push(task_name); }
+    if (task_result !== undefined) { fields.push("task_result = ?"); values.push(task_result); }
+    if (task_cause !== undefined) { fields.push("task_cause = ?"); values.push(task_cause); }
+    if (task_man !== undefined) { fields.push("task_man = ?"); values.push(task_man); }
+    if (task_description !== undefined) { fields.push("task_description = ?"); values.push(task_description); }
+    if (task_date !== undefined) { fields.push("task_date = ?"); values.push(task_date); }
+    if (start_time !== undefined) { fields.push("start_time = ?"); values.push(start_time); }
+    if (end_time !== undefined) { fields.push("end_time = ?"); values.push(end_time); }
+    if (none_time !== undefined) { fields.push("none_time = ?"); values.push(none_time); }
+    if (move_time !== undefined) { fields.push("move_time = ?"); values.push(move_time); }
+    if (group !== undefined) { fields.push("`group` = ?"); values.push(group); }
+    if (site !== undefined) { fields.push("site = ?"); values.push(site); }
+    if (SOP !== undefined) { fields.push("SOP = ?"); values.push(SOP); }
+    if (tsguide !== undefined) { fields.push("tsguide = ?"); values.push(tsguide); }
+    if (line !== undefined) { fields.push("`line` = ?"); values.push(line); }
+    if (warranty !== undefined) { fields.push("warranty = ?"); values.push(warranty); }
+    if (equipment_type !== undefined) { fields.push("equipment_type = ?"); values.push(equipment_type); }
+    if (equipment_name !== undefined) { fields.push("equipment_name = ?"); values.push(equipment_name); }
+    if (workType !== undefined) { fields.push("work_type = ?"); values.push(workType); }
+    if (setupItem !== undefined) { fields.push("setup_item = ?"); values.push(setupItem); }
+    if (maintItem !== undefined) { fields.push("maint_item = ?"); values.push(maintItem); }
+    if (transferItem !== undefined) { fields.push("transfer_item = ?"); values.push(transferItem); }
+    if (task_maint !== undefined) { fields.push("task_maint = ?"); values.push(task_maint); }
+    if (status !== undefined) { fields.push("status = ?"); values.push(status); }
+
+    values.push(id);
+
+    if (fields.length === 0) {
+        throw new Error("No fields to update");
+    }
+
+    const query = `
+        UPDATE work_log SET ${fields.join(', ')}
+        WHERE id = ?
+    `;
+
+    console.log('작업 로그 수정 쿼리:', query);
     console.log('수정할 값:', values);
 
     try {
-        const query = `
-            UPDATE work_log SET
-                task_name = ?, task_result = ?, task_cause = ?, task_man = ?, task_description = ?, task_date = ?, start_time = ?, end_time = ?, none_time = ?, move_time = ?,
-                \`group\` = ?, site = ?, SOP = ?, tsguide = ?, \`line\` = ?, warranty = ?, equipment_type = ?, equipment_name = ?, work_type = ?, setup_item = ?, maint_item = ?, transfer_item = ?, task_maint = ?, status = ?
-            WHERE id = ?
-        `;
         await pool.query(query, values);
         console.log('쿼리 실행 성공');
     } catch (err) {
@@ -70,6 +97,7 @@ exports.updateWorkLog = async (id, task_name, task_result, task_cause, task_man,
         throw err;  // 에러를 다시 던져 컨트롤러에서 잡을 수 있게 합니다.
     }
 };
+
 
 
 
