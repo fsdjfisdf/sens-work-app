0b57ef967b10e7d54217df8a505a27f0c786fe70
jhw <jhw86399@naver.com>
2024-07-29 19:26:03 +0900

    saved18

--- back/src/routes/equipmentRoute.js
+++ back/src/routes/equipmentRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const equipmentController = require('../controllers/equipmentController');
 
-router.get('/equipment', equipmentController.getEquipment);
+router.get('/equipment', equipmentController.getEquipments);
 
 module.exports = router;
--- front/js/equipment.js
+++ front/js/equipment.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     // Fetch equipment data from the server
     async function loadEquipment() {
         try {
-            const response = await axios.get('http://3.37.165.84/equipment');
+            const response = await axios.get('http://3.37.165.84:3001/equipment');
             equipments = response.data;
             displayEquipments(equipments);
         } catch (error) {
