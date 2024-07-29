b33459d054df80766464c08ebd308acc7cbec1eb
jhw <jhw86399@naver.com>
2024-07-29 19:47:06 +0900

    saved18

--- back/src/routes/equipmentRoute.js
+++ back/src/routes/equipmentRoute.js
@@ -5,4 +5,6 @@ const equipmentController = require('../controllers/equipmentController');
 // 장비 목록 조회
 router.get('/equipment', equipmentController.getEquipments);
 
-module.exports = router;
+module.exports = (app) => {
+  app.use('/api', router);
+};
