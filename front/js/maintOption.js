f3ee76406b6fdf5c3f491dc916e3964c5136217c
jhw <jhw86399@naver.com>
2024-08-02 16:07:53 +0900

    saved18

--- front/js/maintOption.js
+++ front/js/maintOption.js
@@ -1,33 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Initialize Select2 for maintOptionSelect and transferOptionSelect
-    $('#maintOptionSelect').select2();
+    // Initialize Select2 for transferOptionSelect
     $('#transferOptionSelect').select2();
 
     const workTypeSelect = document.getElementById('workType');
     const equipmentTypeSelect = document.getElementById('equipment_type');
-    const maintOptionContainer = document.getElementById('maintOptions');
-    const maintOptionSelect = document.getElementById('maintOptionSelect');
     const transferOptionContainer = document.getElementById('transferOptions');
     const transferOptionSelect = document.getElementById('transferOptionSelect');
-
-    const maintOptions = {
-        "SUPRA N": ["SELECT", "LP ESCORT", "EFEM ROBOT TEACHING", "EFEM ROBOT REP", "EFEM ROBOT CONTROLLER", "TM ROBOT TEACHING", "TM ROBOT REP",
-            "TM ROBOT CONTROLLER", "PASSIVE PAD REP", "PIN CYLINDER", "PUSHER CYLINDER", "IB FLOW", "DRT", "FFU CONTROLLER", "FAN", "MOTOR DRIVER",
-            "R1","R3","R5","R3 TO R5", "MICROWAVE", "APPLICATOR", "GENERATOR", "CHUCK", "PROCESS KIT", "HELIUM DETECTOR", "HOOK LIFT PIN", "BELLOWS",
-            "PIN SENSOR", "LM GUIDE", "PIN MOTOR CONTROLLER", "SINGLE EPD", "DUAL EPD", "GAS BOX BOARD", "TEMP CONTROLLER BOARD", "POWER DISTRIBUTiON BOARD",
-            "DC POWER SUPPLY", "BM SENSOR", "PIO SENSOR", "SAFETY MODULE", "D-NET", "MFC", "VALVE", "SOLENOID","FAST VAC VALVE", "SLOW VAC VALVE",
-            "SLIT DOOR", "APC VALVE", "SHUTOFF VALVE", "BARATRON ASS'Y", "PIRANI ASS'Y", "VIEW PORT QUARTZ", "FLOW SWITCH", "CERAMIC PLATE", "MONITOR",
-            "KEYBOARD","MOUSE","CTC","PMC","EDA","EFEM CONTROLLER","S/W PATCH"],
-        "SUPRA XP": ["SELECT"],
-        "INTEGER": ["SELECT", "SWAP KIT", "GAS LINE & GAS FILTER","FLANGE ADAPTOR","SLOT VALVE ASSY(HOUSING)","SLOT VALVE","DOOR VALVE",
-            "PENDULUM VALVE","PIN ASSY MODIFY","MOTOR & CONTROLLER","PIN 구동부 ASSY","SENSOR","STEP MOTOR & CONTROLLER","LL 구동부 ASSY",
-            "EFEM ROBOT REP","TM ROBOT REP", "EFEM ROBOT TEACHING", "TM ROBOT TEACHING","UNDER COVER","VAC LINE","BARATRON GAUGE",
-            "PIRANI GAUGE","MANUAL VALVE","PNEUMATIC VALVE","ISOLATION VALVE","VACUUM BLOCK","CHECK VALVE","EPC","CHUCK","GENERATOR",
-            "AIO CALIBRATION(PSK BOARD)","AIO CALIBRATION(TOS BOARD)","CODED SENSOR","GAS BOX DOOR SENSOR","LASER SENSOR AMP"],
-        "PRECIA": ["SELECT"],
-        "ECOLITE": ["SELECT"],
-        "JENEVA": ["SELECT"]
-    };
 
     const transferOptions = {
         "SUPRA N": ["SELECT", "LP ESCORT", "EFEM ROBOT TEACHING", "EFEM ROBOT REP", "EFEM ROBOT CONTROLLER", "TM ROBOT TEACHING", "TM ROBOT REP",
@@ -48,20 +26,18 @@ document.addEventListener('DOMContentLoaded', function() {
         "JENEVA": ["SELECT"]
     };
 
-    const supraModels = ["SUPRA NM", "SUPRA III", "SUPRA IV", "SUPRA V", "SUPRA Vplus", "SUPRA VM"];
-    const integerModels = ["INTEGER IVr", "INTEGER plus", "INTEGER XP"];
+    function updateTransferOptions() {
+        const workTypeValue = workTypeSelect.value;
+        const equipmentTypeValue = equipmentTypeSelect.value;
 
-    // Assign the same options for SUPRA N to other SUPRA models
-    supraModels.forEach(model => {
-        maintOptions[model] = maintOptions["SUPRA N"];
-        transferOptions[model] = transferOptions["SUPRA N"];
-    });
-
-    // Assign the same options for INTEGER to other INTEGER models
-    integerModels.forEach(model => {
-        maintOptions[model] = maintOptions["INTEGER"];
-        transferOptions[model] = transferOptions["INTEGER"];
-    });
+        if (workTypeValue === 'MAINT') {
+            transferOptionContainer.style.display = 'block';
+            const transferOptionsList = transferOptions[equipmentTypeValue] || ["SELECT"];
+            updateOptions(transferOptionSelect, transferOptionsList);
+        } else {
+            transferOptionContainer.style.display = 'none';
+        }
+    }
 
     function updateOptions(selectElement, options) {
         selectElement.innerHTML = "";
@@ -74,23 +50,11 @@ document.addEventListener('DOMContentLoaded', function() {
         $(selectElement).select2(); // re-initialize Select2 with new options
     }
 
-    function updateMaintOptions() {
-        if (workTypeSelect.value === 'MAINT') {
-            maintOptionContainer.style.display = 'block';
-            transferOptionContainer.style.display = 'none';
-            const maintOptionsList = maintOptions[equipmentTypeSelect.value] || ["SELECT"];
-            updateOptions(maintOptionSelect, maintOptionsList);
-        } else if (workTypeSelect.value === 'TRANSFER') {
-            maintOptionContainer.style.display = 'none';
-            transferOptionContainer.style.display = 'block';
-            const transferOptionsList = transferOptions[equipmentTypeSelect.value] || ["SELECT"];
-            updateOptions(transferOptionSelect, transferOptionsList);
-        } else {
-            maintOptionContainer.style.display = 'none';
-            transferOptionContainer.style.display = 'none';
-        }
-    }
+    workTypeSelect.addEventListener('change', updateTransferOptions);
+    equipmentTypeSelect.addEventListener('change', updateTransferOptions);
 
-    workTypeSelect.addEventListener('change', updateMaintOptions);
-    equipmentTypeSelect.addEventListener('change', updateMaintOptions);
+    transferOptionSelect.addEventListener('change', function() {
+        const transfer_item = transferOptionSelect.value;
+        console.log('Transfer item selected:', transfer_item);
+    });
 });
--- front/js/worklog.js
+++ front/js/worklog.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
 
-    
     function checkLogin() {
         const token = localStorage.getItem('x-access-token');
         if (!token) {
@@ -11,210 +10,208 @@ document.addEventListener('DOMContentLoaded', async () => {
         return true;
     }
 
-    
-  function getTodayDate() {
-      const today = new Date();
-      const year = today.getFullYear();
-      const month = String(today.getMonth() + 1).padStart(2, '0');
-      const day = String(today.getDate()).padStart(2, '0');
-      return `${year}-${month}-${day}`;
-  }
+    function getTodayDate() {
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0');
+        const day = String(today.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
 
-  
+    document.getElementById('task_date').value = getTodayDate();
 
+    document.getElementById('workType').addEventListener('change', function() {
+        const workTypeValue = this.value;
+        const additionalOptions = document.getElementById('additionalOptions');
+        const maintOptions = document.getElementById('maintOptions');
+        const transferOptions = document.getElementById('transferOptions');
+        if (workTypeValue === 'SET UP' || workTypeValue === 'RELOCATION') {
+            additionalOptions.style.display = 'block';
+            maintOptions.style.display = 'none';
+            transferOptions.style.display = 'none';
+        } else if (workTypeValue === 'MAINT') {
+            transferOptions.style.display = 'block'; // MAINT 시 TRANSFER 옵션도 보이게 함
+            additionalOptions.style.display = 'none';
+        } else {
+            additionalOptions.style.display = 'none';
+            maintOptions.style.display = 'none';
+            transferOptions.style.display = 'none';
+        }
+    });
 
-  document.getElementById('task_date').value = getTodayDate();
+    document.getElementById('preview-save').addEventListener('click', () => {
+        const task_date = document.getElementById('task_date').value;
+        const start_time = document.getElementById('start_time').value;
+        const end_time = document.getElementById('end_time').value;
+        const task_name = document.getElementById('task_name').value;
+        const equipment_name = document.getElementById('equipment_name').value;
+        const setupItem = document.getElementById('additionalWorkType').value;
+        const transferItem = document.getElementById('transferOptionSelect').value;
 
-  document.getElementById('workType').addEventListener('change', function() {
-      const workTypeValue = this.value;
-      const additionalOptions = document.getElementById('additionalOptions');
-      const maintOptions = document.getElementById('maintOptions');
-      const transferOptions = document.getElementById('transferOptions');
-      if (workTypeValue === 'SET UP' || workTypeValue === 'RELOCATION') {
-          additionalOptions.style.display = 'block';
-          maintOptions.style.display = 'none';
-          transferOptions.style.display = 'none';
-      } else if (workTypeValue === 'MAINT') {
-          maintOptions.style.display = 'none';
-          transferOptions.style.display = 'block';
-          additionalOptions.style.display = 'none';
-      } else {
-          additionalOptions.style.display = 'none';
-          maintOptions.style.display = 'none';
-          transferOptions.style.display = 'none';
-      }
-  });
+        const taskMans = Array.from(document.querySelectorAll('.task-man-container')).map(container => {
+            const input = container.querySelector('.task-man-input').value;
+            const role = container.querySelector('.task-man-select').value;
+            return `${input} (${role})`;
+        });
 
-  document.getElementById('preview-save').addEventListener('click', () => {
-      const task_date = document.getElementById('task_date').value;
-      const start_time = document.getElementById('start_time').value;
-      const end_time = document.getElementById('end_time').value;
-      const task_name = document.getElementById('task_name').value;
-      const equipment_name = document.getElementById('equipment_name').value;
-      const setupItem = document.getElementById('additionalWorkType').value;
-      const transferItem = document.getElementById('transferOptionSelect').value;
+        const uniqueTaskMans = [...new Set(taskMans)].join(', ');
 
-      const taskMans = Array.from(document.querySelectorAll('.task-man-container')).map(container => {
-          const input = container.querySelector('.task-man-input').value;
-          const role = container.querySelector('.task-man-select').value;
-          return `${input} (${role})`;
-      })
+        document.getElementById('preview-task_date').innerText = task_date;
+        document.getElementById('preview-start_time').innerText = start_time;
+        document.getElementById('preview-end_time').innerText = end_time;
+        document.getElementById('preview-task_name').innerText = task_name;
+        document.getElementById('preview-equipment_name').innerText = equipment_name;
+        document.getElementById('preview-setupItem').innerText = setupItem;
+        document.getElementById('preview-transferItem').innerText = transferItem;
+        document.getElementById('preview-task_man').innerText = uniqueTaskMans;
 
-      const uniqueTaskMans = [...new Set(taskMans)].join(', ');
+        document.getElementById('modal-overlay').classList.add('visible');
+        document.getElementById('preview-modal').classList.add('visible');
+    });
 
-      document.getElementById('preview-task_date').innerText = task_date;
-      document.getElementById('preview-start_time').innerText = start_time;
-      document.getElementById('preview-end_time').innerText = end_time;
-      document.getElementById('preview-task_name').innerText = task_name;
-      document.getElementById('preview-equipment_name').innerText = equipment_name;
-      document.getElementById('preview-setupItem').innerText = setupItem;
-      document.getElementById('preview-transferItem').innerText = transferItem;
-      document.getElementById('preview-task_man').innerText = uniqueTaskMans;
+    document.getElementById('confirm-save').addEventListener('click', () => {
+        document.getElementById('modal-overlay').classList.remove('visible');
+        document.getElementById('preview-modal').classList.remove('visible');
+        document.getElementById('save-button').click();
+    });
 
-      document.getElementById('modal-overlay').classList.add('visible');
-      document.getElementById('preview-modal').classList.add('visible');
-  });
+    document.getElementById('cancel-save').addEventListener('click', () => {
+        document.getElementById('modal-overlay').classList.remove('visible');
+        document.getElementById('preview-modal').classList.remove('visible');
+    });
 
-  document.getElementById('confirm-save').addEventListener('click', () => {
-      document.getElementById('modal-overlay').classList.remove('visible');
-      document.getElementById('preview-modal').classList.remove('visible');
-      document.getElementById('save-button').click();
-  });
+    const form = document.getElementById('worklogForm');
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-  document.getElementById('cancel-save').addEventListener('click', () => {
-      document.getElementById('modal-overlay').classList.remove('visible');
-      document.getElementById('preview-modal').classList.remove('visible');
-  });
+        const task_name = document.getElementById('task_name').value;
+        const status = document.getElementById('status').value;
 
-  const form = document.getElementById('worklogForm');
-  form.addEventListener('submit', async (event) => {
-      event.preventDefault();
+        const taskResults = Array.from(document.getElementsByClassName('task-result-input')).map(input => input.value).join('<br>');
+        const taskCauses = Array.from(document.getElementsByClassName('task-cause-input')).map(input => input.value).join('<br>');
+        let taskMans = Array.from(document.querySelectorAll('.task-man-container')).map((container, index) => {
+            const input = container.querySelector('.task-man-input').value;
+            const role = container.querySelector('.task-man-select').value;
+            return `${input}(${role})`;
+        });
+        taskMans = [...new Set(taskMans)].join(', ');
+        const taskDescriptions = Array.from(document.getElementsByClassName('task-description-input')).map(input => input.value).join('<br>');
 
-      const task_name = document.getElementById('task_name').value;
-      const status = document.getElementById('status').value;
+        let task_date = document.getElementById('task_date').value;
+        let start_time = document.getElementById('start_time').value;
+        let end_time = document.getElementById('end_time').value;
+        const noneTime = document.getElementById('noneTime').value;
+        const moveTime = document.getElementById('moveTime').value;
 
-      const taskResults = Array.from(document.getElementsByClassName('task-result-input')).map(input => input.value).join('<br>');
-      const taskCauses = Array.from(document.getElementsByClassName('task-cause-input')).map(input => input.value).join('<br>');
-      let taskMans = Array.from(document.querySelectorAll('.task-man-container')).map((container, index) => {
-          const input = container.querySelector('.task-man-input').value;
-          const role = container.querySelector('.task-man-select').value;
-          return `${input}(${role})`;
-      });
-      taskMans = [...new Set(taskMans)].join(', ');
-      const taskDescriptions = Array.from(document.getElementsByClassName('task-description-input')).map(input => input.value).join('<br>');
+        if (!task_date) {
+            task_date = getTodayDate();
+        }
+        if (!start_time) {
+            start_time = '00:00:00';
+        } else {
+            start_time = `${start_time}:00`;
+        }
+        if (!end_time) {
+            end_time = '00:00:00';
+        } else {
+            end_time = `${end_time}:00`;
+        }
 
-      let task_date = document.getElementById('task_date').value;
-      let start_time = document.getElementById('start_time').value;
-      let end_time = document.getElementById('end_time').value;
-      const noneTime = document.getElementById('noneTime').value;
-      const moveTime = document.getElementById('moveTime').value;
+        const group = document.getElementById('group').value;
+        const site = document.getElementById('site').value;
+        const line = document.getElementById('line').value;
+        const SOP = document.getElementById('SOP').value;
+        const tsguide = document.getElementById('tsguide').value;
+        const warranty = document.getElementById('warranty').value;
+        const equipment_type = document.getElementById('equipment_type').value;
+        const equipment_name = document.getElementById('equipment_name').value;
+        const workType = document.getElementById('workType').value;
+        const setupItem = (workType === 'SET UP' || workType === 'RELOCATION') ? document.getElementById('additionalWorkType').value : 'SELECT';
+        const maintItem = (workType === 'MAINT') ? document.getElementById('maintOptionSelect').value : 'SELECT';
+        const transferItem = (workType === 'MAINT') ? document.getElementById('transferOptionSelect').value : 'SELECT';
+        const task_maint = maintItem; // 새로 추가된 필드
 
-      if (!task_date) {
-          task_date = getTodayDate();
-      }
-      if (!start_time) {
-          start_time = '00:00:00';
-      } else {
-          start_time = `${start_time}:00`;
-      }
-      if (!end_time) {
-          end_time = '00:00:00';
-      } else {
-          end_time = `${end_time}:00`;
-      }
+        console.log('전송 데이터:', {
+            task_name,
+            task_result: taskResults,
+            task_cause: taskCauses,
+            task_man: taskMans,
+            task_description: taskDescriptions,
+            task_date,
+            start_time,
+            end_time,
+            noneTime,
+            moveTime,
+            group,
+            site,
+            SOP,
+            tsguide,
+            warranty,
+            line,
+            equipment_type,
+            equipment_name,
+            workType,
+            setupItem,
+            maintItem,
+            transferItem, // 추가된 필드
+            task_maint, // 새로 추가된 필드
+            status
+        });
 
-      const group = document.getElementById('group').value;
-      const site = document.getElementById('site').value;
-      const line = document.getElementById('line').value;
-      const SOP = document.getElementById('SOP').value;
-      const tsguide = document.getElementById('tsguide').value;
-      const warranty = document.getElementById('warranty').value;
-      const equipment_type = document.getElementById('equipment_type').value;
-      const equipment_name = document.getElementById('equipment_name').value;
-      const workType = document.getElementById('workType').value;
-      const setupItem = (workType === 'SET UP' || workType === 'RELOCATION') ? document.getElementById('additionalWorkType').value : 'SELECT';
-      const maintItem = workType === 'MAINT' ? document.getElementById('maintOptionSelect').value : 'SELECT';
-      const transferItem = workType === 'MAINT' ? document.getElementById('transferOptionSelect').value : 'SELECT';
-      const task_maint = maintItem; // 새로 추가된 필드
+        try {
+            const response = await axios.post(`http://3.37.165.84:3001/log`, {
+                task_name,
+                task_result: taskResults,
+                task_cause: taskCauses,
+                task_man: taskMans,
+                task_description: taskDescriptions,
+                task_date,
+                start_time,
+                end_time,
+                none_time: noneTime,
+                move_time: moveTime,
+                group,
+                site,
+                SOP,
+                tsguide,
+                warranty,
+                line,
+                equipment_type,
+                equipment_name,
+                workType,
+                setupItem,
+                maintItem,
+                transferItem, // 추가된 필드
+                task_maint, // 새로 추가된 필드
+                status
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
 
-      console.log('전송 데이터:', {
-          task_name,
-          task_result: taskResults,
-          task_cause: taskCauses,
-          task_man: taskMans,
-          task_description: taskDescriptions,
-          task_date,
-          start_time,
-          end_time,
-          noneTime,
-          moveTime,
-          group,
-          site,
-          SOP,
-          tsguide,
-          warranty,
-          line,
-          equipment_type,
-          equipment_name,
-          workType,
-          setupItem,
-          maintItem,
-          transferItem, // 추가된 필드
-          task_maint, // 새로 추가된 필드
-          status
-      });
+            if (response.status === 201) {
+                alert('작업 이력 추가 성공');
+                loadWorkLogs();
+            }
+        } catch (error) {
+            console.error('작업 이력 추가 실패:', error);
+        }
+    });
 
-      try {
-          const response = await axios.post(`http://3.37.165.84:3001/log`, {
-              task_name,
-              task_result: taskResults,
-              task_cause: taskCauses,
-              task_man: taskMans,
-              task_description: taskDescriptions,
-              task_date,
-              start_time,
-              end_time,
-              none_time: noneTime,
-              move_time: moveTime,
-              group,
-              site,
-              SOP,
-              tsguide,
-              warranty,
-              line,
-              equipment_type,
-              equipment_name,
-              workType,
-              setupItem,
-              maintItem,
-              transferItem, // 추가된 필드
-              task_maint, // 새로 추가된 필드
-              status
-          }, {
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-          });
+    if (checkLogin()) {
+        await loadEngineers();
+        await loadWorkLogs();
+        renderCalendar(logs, engineers, currentYear, currentMonth);
+    }
 
-          if (response.status === 201) {
-              alert('작업 이력 추가 성공');
-              loadWorkLogs();
-          }
-      } catch (error) {
-      }
-  });
-  if (checkLogin()) {
-    await loadEngineers();
-    await loadWorkLogs();
-    renderCalendar(logs, engineers, currentYear, currentMonth);
-}
-  const signOutButton = document.querySelector("#sign-out");
+    const signOutButton = document.querySelector("#sign-out");
 
-  if (signOutButton) {
-      signOutButton.addEventListener("click", function() {
-          localStorage.removeItem("x-access-token"); // JWT 토큰 삭제
-          alert("로그아웃 되었습니다.");
-          window.location.replace("./signin.html"); // 로그인 페이지로 리디렉션
-      });
-  }
+    if (signOutButton) {
+        signOutButton.addEventListener("click", function() {
+            localStorage.removeItem("x-access-token"); // JWT 토큰 삭제
+            alert("로그아웃 되었습니다.");
+            window.location.replace("./signin.html"); // 로그인 페이지로 리디렉션
+        });
+    }
 });
