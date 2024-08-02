0299554bc6a2d0245d687b8b18df396edd9b75b0
jhw <jhw86399@naver.com>
2024-08-02 10:02:10 +0900

    saved18

--- back/src/controllers/equipmentController.js
+++ back/src/controllers/equipmentController.js
@@ -12,7 +12,6 @@ exports.getEquipments = async (req, res) => {
     }
 
     const [rows] = await pool.query(query, params);
-    console.log('Database query result:', rows); // 데이터베이스 쿼리 결과를 출력
     res.status(200).json(rows);
   } catch (err) {
     console.error('Error retrieving equipment data:', err);
