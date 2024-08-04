fa0c7749ffd5076fa5644259cd0b03f4462f1df7
jhw <jhw86399@naver.com>
2024-08-05 08:33:05 +0900

    saved18

--- front/js/maintOption.js
+++ front/js/maintOption.js
@@ -15,16 +15,29 @@ document.addEventListener('DOMContentLoaded', function() {
         "DC POWER SUPPLY", "BM SENSOR", "PIO SENSOR", "SAFETY MODULE", "D-NET", "MFC", "VALVE", "SOLENOID","FAST VAC VALVE", "SLOW VAC VALVE",
         "SLIT DOOR", "APC VALVE", "SHUTOFF VALVE", "BARATRON ASS'Y", "PIRANI ASS'Y", "VIEW PORT QUARTZ", "FLOW SWITCH", "CERAMIC PLATE", "MONITOR",
         "KEYBOARD","MOUSE","CTC","PMC","EDA","EFEM CONTROLLER","S/W PATCH"],
-        "SUPRA XP": ["SELECT", "TRANSFER OPTION 1"],
-        "INTEGER": ["SELECT", "SWAP KIT", "GAS LINE & GAS FILTER","FLANGE ADAPTOR","SLOT VALVE ASSY(HOUSING)","SLOT VALVE","DOOR VALVE",
+        "SUPRA XP": ["이관항목 없음"],
+        "INTEGER plus": ["SELECT", "SWAP KIT", "GAS LINE & GAS FILTER","FLANGE ADAPTOR","SLOT VALVE ASSY(HOUSING)","SLOT VALVE","DOOR VALVE",
             "PENDULUM VALVE","PIN ASSY MODIFY","MOTOR & CONTROLLER","PIN 구동부 ASSY","SENSOR","STEP MOTOR & CONTROLLER","LL 구동부 ASSY",
             "EFEM ROBOT REP","TM ROBOT REP", "EFEM ROBOT TEACHING", "TM ROBOT TEACHING","UNDER COVER","VAC LINE","BARATRON GAUGE",
             "PIRANI GAUGE","MANUAL VALVE","PNEUMATIC VALVE","ISOLATION VALVE","VACUUM BLOCK","CHECK VALVE","EPC","CHUCK","GENERATOR",
             "AIO CALIBRATION(PSK BOARD)","AIO CALIBRATION(TOS BOARD)","CODED SENSOR","GAS BOX DOOR SENSOR","LASER SENSOR AMP"],
-        "PRECIA": ["SELECT"],
-        "ECOLITE": ["SELECT"],
-        "JENEVA": ["SELECT"]
+        "PRECIA": ["이관항목 없음"],
+        "ECOLITE": ["이관항목 없음"],
+        "JENEVA": ["이관항목 없음"]
     };
+
+    const supraModels = ["SUPRA NM", "SUPRA III", "SUPRA IV", "SUPRA V", "SUPRA Vplus", "SUPRA VM"];
+    const integerModels = ["INTEGER IVr", "INTEGER plus", "INTEGER XP"];
+
+    // Assign the same options for SUPRA N to other SUPRA models
+    supraModels.forEach(model => {
+        transferOptions[model] = transferOptions["SUPRA N"];
+    });
+
+    // Assign the same options for INTEGER to other INTEGER models
+    integerModels.forEach(model => {
+        transferOptions[model] = transferOptions["INTEGER plus"];
+    });
 
     function updateTransferOptions() {
         const workTypeValue = workTypeSelect.value;
