f52c91719640184d09c6fc558814206cc3a37001
jhw <jhw86399@naver.com>
2024-10-18 14:50:33 +0900

    saved18

--- back/src/dao/work_LogDao.js
+++ back/src/dao/work_LogDao.js
@@ -157,11 +157,11 @@ try {
 };
 
 
-exports.getWorkLogs = async (equipment_type) => {
+exports.getSupraXPWorkLogs = async (equipment_type) => {
   const connection = await pool.getConnection(async conn => conn);
   try {
       let query = 'SELECT * FROM work_log WHERE equipment_type = ? ORDER BY task_date DESC, id DESC';
-      const [rows] = await connection.query(query, [equipment_type]);  // equipment_type 값을 전달하여 필터링
+      const [rows] = await connection.query(query, [equipment_type]);  // 'SUPRA XP'만 필터링
       connection.release();
       return rows;
   } catch (err) {
