6d15566394b2e0d6ca5ddb395a4bb9136f0b4771
jhw <jhw86399@naver.com>
2024-10-25 14:19:43 +0900

    saved18

--- back/config/express.js
+++ back/config/express.js
@@ -62,6 +62,7 @@ module.exports = function () {
   require("../src/routes/integerSetupRoute")(app);  // 추가된 라우트
   require("../src/routes/preciaSetupRoute")(app);  // 추가된 라우트
   require("../src/routes/ecoliteSetupRoute")(app);  // 추가된 라우트
+  require("../src/routes/genevaSetupRoute")(app);  // 추가된 라우트
   require("../src/routes/supraxpSetupRoute")(app);  // 추가된 라우트
 
   require("../src/routes/integerMaintenanceRoute")(app);  // 추가된 라우트
