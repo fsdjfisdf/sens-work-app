091778e686ac6de8caf1d8cfb82d86d893feab3c
jhw <jhw86399@naver.com>
2024-12-12 15:55:36 +0900

    saved19

--- back/src/routes/AIRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const AIController = require("../controllers/AIController");
-
-const router = express.Router();
-
-// AI Query 처리 라우트
-router.post("/query", AIController.processQuery);
-
-module.exports = router;
