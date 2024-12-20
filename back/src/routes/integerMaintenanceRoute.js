4d461a55ba2f5fe5c9ea89f6b560c74f9215a4a4
jhw <jhw86399@naver.com>
2024-12-20 14:08:24 +0900

    saved21

--- back/src/routes/integerMaintenanceRoute.js
+++ back/src/routes/integerMaintenanceRoute.js
@@ -1,13 +1,13 @@
-const integerMaintenanceController = require("../controllers/integerMaintenanceController");
+const integermaintenanceController = require("../controllers/integerMaintController");
 const jwtMiddleware = require("../../config/jwtMiddleware");
 
 module.exports = function (app) {
   // 체크리스트 불러오기
-  app.get("/integer-maintenance", jwtMiddleware, integerMaintenanceController.getChecklist);
+  app.get("/integer-maintenance", jwtMiddleware, integermaintenanceController.getChecklist);
 
   // 체크리스트 저장
-  app.post("/integer-maintenance", jwtMiddleware, integerMaintenanceController.saveChecklist);
+  app.post("/integer-maintenance", jwtMiddleware, integermaintenanceController.saveChecklist);
 
   // 모든 체크리스트 불러오기
-  app.get("/integer-maintenance/all", jwtMiddleware, integerMaintenanceController.getAllChecklists);
+  app.get("/integer-maintenance/all", jwtMiddleware, integermaintenanceController.getAllChecklists);
 };
