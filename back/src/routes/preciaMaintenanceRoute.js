8c59234effdacc198ae5548fdbc61659dbac694b
jhw <jhw86399@naver.com>
2025-03-28 13:59:38 +0900

    saved21

--- back/src/routes/preciaMaintenanceRoute.js
+++ back/src/routes/preciaMaintenanceRoute.js
@@ -1,9 +1,9 @@
-const preicamaintenanceController = require("../controllers/preciaMaintenanceController");
+const preciamaintenanceController = require("../controllers/preciaMaintenanceController");
 const jwtMiddleware = require("../../config/jwtMiddleware");
 
 module.exports = function (app) {
   // 체크리스트 불러오기
-  app.get("/preica-maintenance", jwtMiddleware, preciamaintenanceController.getChecklist);
+  app.get("/precia-maintenance", jwtMiddleware, preciamaintenanceController.getChecklist);
 
   // 체크리스트 저장
   app.post("/precia-maintenance", jwtMiddleware, preciamaintenanceController.saveChecklist);
@@ -11,3 +11,4 @@ module.exports = function (app) {
   // 모든 체크리스트 불러오기
   app.get("/precia-maintenance/all", jwtMiddleware, preciamaintenanceController.getAllChecklists);
 };
+
