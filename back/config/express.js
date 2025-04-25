dce60c12e0ff38bfe5e453068896063cf69f6f83
jhw <jhw86399@naver.com>
2025-04-25 15:41:03 +0900

    saved21

--- back/config/express.js
+++ back/config/express.js
@@ -99,7 +99,8 @@ const editRoutes = require("../src/routes/editRoute"); // 🔹 작업 이력 편
 
 // 기존 코드의 라우트 설정 부분에 추가
 app.use("/api", editRoutes); // 🔹 작업 이력 편집 API 라우트 연결
-app.use('/api/test', require('./routes/testRoute'));
+const testRoute = require('./routes/testRoute');
+app.use('/api/test', testRoute);
 
   
 
