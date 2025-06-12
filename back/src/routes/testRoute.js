26f1ec42735ff75ca6fe33da5bcfceb42799a76e
jhw <jhw86399@naver.com>
2025-06-12 11:12:34 +0900

    saved21

--- back/src/routes/testRoute.js
+++ back/src/routes/testRoute.js
@@ -1,12 +1,12 @@
+// src/routes/testRoute.js
 const express = require("express");
 const router = express.Router();
 const testController = require("../controllers/testController");
-const jwtMiddleware = require("../../jwtMiddleware"); // ✅ JWT 인증
 
 module.exports = function (app) {
-  app.use("/api/test", router);
+  app.use("/api/test", router); // ✅ 여기서만 router 사용
 
   router.get("/questions", testController.getQuestions);
-  router.post("/submit-test", jwtMiddleware, testController.submitTest);  // ✅ 여기에 JWT 미들웨어 추가
-  router.get("/test-results", jwtMiddleware, testController.getTestResults);
+  router.post("/submit-test", jwtMiddleware, testController.submitTest);
+  router.get("/test-results", testController.getTestResults);
 };
