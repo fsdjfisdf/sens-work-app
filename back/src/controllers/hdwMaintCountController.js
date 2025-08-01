d43e91fe133a28dc6140eb8be69faea18e78d90e
jhw <jhw86399@naver.com>
2025-08-01 10:51:09 +0900

    saved38

---
+++ back/src/controllers/hdwMaintCountController.js
@@ -0,0 +1,25 @@
+const { pool } = require('../../config/database');
+
+// 작업 카운트 데이터를 가져오는 함수
+exports.getTaskCount = async (req, res) => {
+  console.log('Request received for /hdw-task-count');
+  try {
+      const [rows] = await pool.query('SELECT * FROM HDW_MAINT_COUNT');
+      res.status(200).json(rows);
+  } catch (err) {
+      console.error('Error retrieving task count data:', err);
+      res.status(500).json({ error: 'Error retrieving task count data' });
+  }
+};
+
+exports.saveAggregatedData = async (req, res) => {
+    try {
+      const data = req.body;
+      // Logic to save aggregated data goes here.
+      res.status(201).json({ message: 'Aggregated data saved successfully' });
+    } catch (err) {
+      console.error('Error saving aggregated data:', err);
+      res.status(500).json({ error: 'Error saving aggregated data' });
+    }
+  };
+  
