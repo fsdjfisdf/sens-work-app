2b6f446d9c761ad3cde8944b0fc0e6fc5ad35a8c
jhw <jhw86399@naver.com>
2025-09-19 10:10:21 +0900

    saved39

--- front/js/equipmentwarranty.js
+++ front/js/equipmentwarranty.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // SITE별 LINE 옵션
     const lineOptions = {
         "PT": ["P1F", "P1D", "P2F", "P2D", "P2-S5", "P3F", "P3D", "P3-S5", "P4F", "P4D", "P4-S5"],
-        "HS": ["12L", "13L", "15L", "16L", "17L", "S1", "S3", "S4", "S3V", "NRD", "NRDK", "NRD-V", "U4", "M1", "5L"],
+        "HS": ["1L", "12L", "13L", "15L", "16L", "17L", "S1", "S3", "S4", "S3V", "NRD", "NRDK", "NRD-V", "U4", "M1", "5L"],
         "IC": ["M10", "M14", "M16", "R3"],
         "CJ": ["M11", "M12", "M15"],
         "PSKH": ["PSKH", "C1", "C2", "C3", "C5"],
@@ -86,6 +86,7 @@ document.addEventListener('DOMContentLoaded', function () {
         "China-Shanghai": ["GTX"],
         "China-Beijing": ["JIDIAN"],
         "Taiwan-Taichoung": ["MICRON"],
+        "Taiwan-Linkou": ["MICRON"],
         "Singapore": ["MICRON"],
         "Training": ["Training"]
     };
