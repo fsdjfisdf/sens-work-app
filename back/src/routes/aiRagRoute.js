0cc7a1a1caa4be1409dddb05a883c26afd92fa2a
jhw <jhw86399@naver.com>
2025-11-03 16:45:35 +0900

    saved45

--- back/src/routes/aiRagRoute.js
+++ back/src/routes/aiRagRoute.js
@@ -1,9 +1,9 @@
-// back/routes/aiRagRoute.js
+// back/src/routes/aiRagRoute.js
 const express = require('express');
 const router = express.Router();
 
-// 컨트롤러 경로: back/src/controllers/aiRagController.js
-const { ask } = require('../src/controllers/aiRagController');
+ // 컨트롤러 경로: back/src/controllers/aiRagController.js
+ const { ask } = require('../controllers/aiRagController');
 
 // POST /api/rag/ask
 router.post('/ask', ask);
